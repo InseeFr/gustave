57891e427c721175d00c3dddd8ab5861345b7d3a
Martin Chevalier <mc@slmc.fr>
2017-09-03 18:53:41 +0200

    Reflowing comments in variance_function.R

--- R/variance_function.R
+++ R/variance_function.R
@@ -2,57 +2,67 @@
 
 #' Linear Regression Residuals Calculation
 #'
-#' @description \code{rescal} calculates linear regression residuals in an efficient way :
-#' handling several dependant variables at a time, using Matrix::TsparseMatrix
-#' capabilities and allowing for precalculation of the matrix inverse.
+#' @description \code{rescal} calculates linear regression residuals in an 
+#' efficient way : handling several dependant variables at a time, using 
+#' Matrix::TsparseMatrix capabilities and allowing for precalculation of 
+#' the matrix inverse.
 #'
-#' @param y A numerical matrix of dependant variable(s). May be a Matrix::TsparseMatrix.
-#' @param x A numerical matrix of independant variable(s). May be a Matrix::TsparseMatrix.
+#' @param y A numerical matrix of dependant variable(s). May be a 
+#' Matrix::TsparseMatrix.
+#' @param x A numerical matrix of independant variable(s). May be a 
+#' Matrix::TsparseMatrix.
 #' @param w An optional numerical vector of row weights.
 #' @param by An optional categorical vector (factor or character)
-#' when residuals calculation is to be conducted within by-groups (see Details).
-#' @param colinearity.check A boolean (\code{TRUE} or \code{FALSE}) or \code{NULL} indicating
-#' whether to perform a check for colinearity or not (see Details).
+#' when residuals calculation is to be conducted within by-groups 
+#' (see Details).
+#' @param colinearity.check A boolean (\code{TRUE} or \code{FALSE}) or 
+#' \code{NULL} indicating whether to perform a check for colinearity or 
+#' not (see Details).
 #' @param precalc A list of precalculated results (see Details).
 #'
-#' @details In the context of the \code{gustave} package, linear regression residual
-#' calculation is solely used to take into account the effect of calibration
-#' on variance estimation. Independant variables are therefore most likely to be the
-#' same from one variance estimation to another, hence the inversion of the matrix
-#' \code{t(x) \%*\% Diagonal(x = w) \%*\% x} can be done once and for all at a precalculation step.
+#' @details In the context of the \code{gustave} package, linear 
+#' regression residual calculation is solely used to take into account 
+#' the effect of calibration on variance estimation. Independant variables 
+#' are therefore most likely to be the same from one variance estimation 
+#' to another, hence the inversion of the matrix 
+#' \code{t(x) \%*\% Diagonal(x = w) \%*\% x} can be done once and for all 
+#' at a precalculation step.
 #'
-#' The parameters \code{y} and \code{precalc} determine whether a list of precalculated
-#' data should be used in order to speed up the regression residuals computation at execution
-#' time:
+#' The parameters \code{y} and \code{precalc} determine whether a list of 
+#' precalculated data should be used in order to speed up the regression 
+#' residuals computation at execution time:
 #' \itemize{
-#'  \item if \code{y} not \code{NULL} and \code{precalc} \code{NULL} : on-the-fly
-#'  calculation of the matrix inverse and the regression residuals (no precalculation).
-#'  \item if \code{y} \code{NULL} and \code{precalc} \code{NULL} : precalculation
-#'  of the matrix inverse which is stored in a list of precalculated data.
-#'  \item if \code{y} not \code{NULL} and \code{precalc} not \code{NULL} : calculation
-#'  of the regression residuals using the list of precalculated data.
+#'  \item if \code{y} not \code{NULL} and \code{precalc} \code{NULL} : 
+#'  on-the-fly calculation of the matrix inverse and the regression residuals 
+#'  (no precalculation).
+#'  \item if \code{y} \code{NULL} and \code{precalc} \code{NULL} : 
+#'  precalculation of the matrix inverse which is stored in a list of 
+#'  precalculated data.
+#'  \item if \code{y} not \code{NULL} and \code{precalc} not \code{NULL} : 
+#'  calculation of the regression residuals using the list of precalculated 
+#'  data.
 #' }
 #'
-#' The \code{by} paramater allows for calculation within by-groups : all calculation are
-#' made separately for each by-group (when calibration was conducted separately on several
-#' subsamples), but in an efficient way using Matrix::TsparseMatrix capabilities
-#' (especiellay when the matrix inverse is precalculated).
+#' The \code{by} paramater allows for calculation within by-groups : all 
+#' calculation are made separately for each by-group (when calibration was 
+#' conducted separately on several subsamples), but in an efficient way using 
+#' Matrix::TsparseMatrix capabilities (especiellay when the matrix inverse is 
+#' precalculated).
+#' 
+#' If \code{colinearity.check} is \code{NULL}, a test for colinearity in the 
+#' independant variables (\code{x}) is conducted if and only if \code{det(t(x)
+#' \%*\% x) == 0}.
+#' 
 #'
-#' If \code{colinearity.check} is \code{NULL}, a test for colinearity in the independant
-#' variables (\code{x}) is conducted only if \code{det(t(x) \%*\% x) == 0)}.
-#'
-#'
-#'
-#' @return \itemize{
-#'  \item if \code{y} is not \code{NULL} (calculation step) : a numerical matrix with same structure
-#'  (regular base::matrix or Matrix::TsparseMatrix) and dimensions as \code{y}.
-#'  \item if \code{y} is \code{NULL} (precalculation step) : a list containing precalculated data: \itemize{
-#'    \item \code{x}: the numerical matrix of independant variables.
-#'    \item \code{w}: the numerical vector of row weights (vector of 1 by default).
-#'    \item \code{inv}: the inverse of \code{t(x) \%*\% Matrix::Diagonal(x = w) \%*\% x}
-#'  }
-#' }
-#'
+#' @return \itemize{ \item if \code{y} is not \code{NULL} (calculation step) : a
+#'   numerical matrix with same structure (regular base::matrix or
+#'   Matrix::TsparseMatrix) and dimensions as \code{y}. \item if \code{y} is
+#'   \code{NULL} (precalculation step) : a list containing precalculated data:
+#'   \itemize{ \item \code{x}: the numerical matrix of independant variables. 
+#'   \item \code{w}: the numerical vector of row weights (vector of 1 by
+#'   default). \item \code{inv}: the inverse of \code{t(x) \%*\%
+#'   Matrix::Diagonal(x = w) \%*\% x} } }
+#'   
 #' @author Martin Chevalier (Insee)
 #'
 #' @seealso \code{\link{block_matrix}} \code{\link{varDT}}
@@ -131,99 +141,101 @@ rescal <- function(y = NULL, x, w = NULL, by = NULL, colinearity.check = NULL, p
 # microbenchmark(rescal(y,x,w),rescal(y,x,w,inv),times = 10)
 
 #' Variance approximation with Deville-Tillé (2005) formula
-#'
-#' @description \code{varDT} estimates the variance of the total in the case
-#' of a balanced sampling design with equal or unequal probabilities. Without
-#' balancing variables, it falls back to Deville's (1993) classical approximation.
-#' Without balancing variables and with equal probabilities, it falls back to
-#' the classical variance estimator for the total in the case of simple random
-#' sampling. Stratification is natively supported.
-#'
-#' @param y A numerical matrix of the variable(s) whose variance of their total is to be estimated. variable(s). May be a Matrix::TsparseMatrix.
+#' 
+#' @description \code{varDT} estimates the variance of the total in the case of
+#'   a balanced sampling design with equal or unequal probabilities. Without 
+#'   balancing variables, it falls back to Deville's (1993) classical
+#'   approximation. Without balancing variables and with equal probabilities, it
+#'   falls back to the classical variance estimator for the total in the case of
+#'   simple random sampling. Stratification is natively supported.
+#'   
+#' @param y A numerical matrix of the variable(s) whose variance of their total
+#'   is to be estimated. variable(s). May be a Matrix::TsparseMatrix.
 #' @param pik A numerical vector of first-order inclusion probabilties.
-#' @param x An optional numerical matrix of balancing variable(s). May be a Matrix::TsparseMatrix.
-#' @param strata An optional categorical vector (factor or character)
-#' when variance estimation is to be conducted within strata.
+#' @param x An optional numerical matrix of balancing variable(s). May be a
+#'   Matrix::TsparseMatrix.
+#' @param strata An optional categorical vector (factor or character) when
+#'   variance estimation is to be conducted within strata.
 #' @param w An optional numerical vector of row weights (see Details).
-#' @param colinearity.check A boolean (\code{TRUE} or \code{FALSE}) or \code{NULL} indicating
-#' whether to perform a check for colinearity or not (see Details).
+#' @param colinearity.check A boolean (\code{TRUE} or \code{FALSE}) or
+#'   \code{NULL} indicating whether to perform a check for colinearity or not
+#'   (see Details).
 #' @param precalc A list of precalculated results (see Details).
-#'
-#' @details \code{varDT} is the workhorse of most variance estimation conducted with
-#' the \code{gustave} package. It might be used to estimate the variance of a total in
-#' the case of (stratified) simple random sampling, (stratified) unequal probability
-#' sampling and (stratified) balanced sampling. The native integration of stratification
-#' based on Matrix::TsparseMatrix allows for significant performance gains compared to
-#' higher level vectorizations (\code{*apply} especially).
-#'
-#' Several time-consuming operations (e.g. colinearity-check, matrix inversion) can be
-#' precalculated in order to speed up the estimation at execution time. This is determined
-#' by the value of the parameters \code{y} and \code{precalc}:
-#' \itemize{
-#'  \item if \code{y} not \code{NULL} and \code{precalc} \code{NULL} : on-the-fly
-#'  calculation (no precalculation).
-#'  \item if \code{y} \code{NULL} and \code{precalc} \code{NULL} : precalculation
-#'  whose results are stored in a list of precalculated data.
-#'  \item if \code{y} not \code{NULL} and \code{precalc} not \code{NULL} : calculation
-#'  using the list of precalculated data.
-#' }
-#'
-#' If \code{colinearity.check} is \code{NULL}, a test for colinearity in the independant
-#' variables (\code{x}) is conducted only if \code{det(t(x) \%*\% x) == 0)}.
-#'
-#' \code{w} is a row weight used at the final summation step. It is useful when \code{varDT}
-#' is used on the second stage of a two-stage sampling design applying the Rao (1975) formula.
-#'
+#'   
+#' @details \code{varDT} is the workhorse of most variance estimation conducted
+#'   with the \code{gustave} package. It might be used to estimate the variance
+#'   of a total in the case of (stratified) simple random sampling, (stratified)
+#'   unequal probability sampling and (stratified) balanced sampling. The native
+#'   integration of stratification based on Matrix::TsparseMatrix allows for
+#'   significant performance gains compared to higher level vectorizations
+#'   (\code{*apply} especially).
+#'   
+#'   Several time-consuming operations (e.g. colinearity-check, matrix
+#'   inversion) can be precalculated in order to speed up the estimation at
+#'   execution time. This is determined by the value of the parameters \code{y}
+#'   and \code{precalc}: \itemize{ \item if \code{y} not \code{NULL} and
+#'   \code{precalc} \code{NULL} : on-the-fly calculation (no precalculation). 
+#'   \item if \code{y} \code{NULL} and \code{precalc} \code{NULL} :
+#'   precalculation whose results are stored in a list of precalculated data. 
+#'   \item if \code{y} not \code{NULL} and \code{precalc} not \code{NULL} :
+#'   calculation using the list of precalculated data. }
+#'   
+#'   If \code{colinearity.check} is \code{NULL}, a test for colinearity in the
+#'   independant variables (\code{x}) is conducted only if \code{det(t(x) \%*\%
+#'   x) == 0)}.
+#'   
+#'   \code{w} is a row weight used at the final summation step. It is useful
+#'   when \code{varDT} is used on the second stage of a two-stage sampling
+#'   design applying the Rao (1975) formula.
+#'   
 #' @section Difference with \code{varest}:
-#'
-#' \code{varDT} differs from \code{sampling::varest} in several ways:
-#' \itemize{
-#'  \item The formula implemented in \code{varDT} is more general and encompasses balanced sampling.
-#'  \item \code{varDT} does not natively implements the calibration estimator (i.e. the
-#'  sampling variance estimator that takes into account the effect of calibration).
-#'  In the context of the \code{gustave} package, \code{\link{rescal}} could be called before
-#'  \code{varDT} in order to achieve the same result.
-#'  \item Even in its reduced form (without balancing variables), the formula implemented in
-#'  \code{varDT} slightly differs from the one implemented in \code{sampling::varest}. Caron,
-#'  Deville and Sautory (1998, pp. 7-8) compares the two estimators (\code{sampling::varest}
-#'  implements V_2, \code{varDT} implements V_1).
-#'  \item \code{varDT} introduces several optimizations: \itemize{
-#'    \item matrixwise operations allow to estimate variance on several interest variables at once
-#'    \item Matrix::TsparseMatrix capability and the native integration of stratification
-#'    induce significant performance gains.
-#'    \item the ability to precalculate some time-consuming operations speeds up the estimation
-#'    at execution time.
-#'  }
-#' }
-#'
-#'
-#' @return \itemize{
-#'  \item if \code{y} is not \code{NULL} (calculation step) : a numerical vector of sizz
-#'  the number of columns of y.
-#'  \item if \code{y} is \code{NULL} (precalculation step) : a list containing precalculated data: \itemize{
-#'    \item \code{pik}: the numerical vector of first-order inclusion probabilities.
-#'    \item \code{A}: the numerical matrix denoted A in (Deville, Tillé, 2005).
-#'    \item \code{ck}: the numerical vector denoted ck2 in (Deville, Tillé, 2005).
-#'    \item \code{inv}: the inverse of \code{A \%*\% Matrix::Diagonal(x = ck) \%*\% t(A)}
-#'    \item \code{diago}: the diagonal term of the variance estimator
-#'  }
-#' }
-#'
+#'   
+#'   \code{varDT} differs from \code{sampling::varest} in several ways: 
+#'   \itemize{ \item The formula implemented in \code{varDT} is more general and
+#'   encompasses balanced sampling. \item \code{varDT} does not natively
+#'   implements the calibration estimator (i.e. the sampling variance estimator
+#'   that takes into account the effect of calibration). In the context of the
+#'   \code{gustave} package, \code{\link{rescal}} could be called before 
+#'   \code{varDT} in order to achieve the same result. \item Even in its reduced
+#'   form (without balancing variables), the formula implemented in \code{varDT}
+#'   slightly differs from the one implemented in \code{sampling::varest}.
+#'   Caron, Deville and Sautory (1998, pp. 7-8) compares the two estimators
+#'   (\code{sampling::varest} implements V_2, \code{varDT} implements V_1). 
+#'   \item \code{varDT} introduces several optimizations: \itemize{ \item
+#'   matrixwise operations allow to estimate variance on several interest
+#'   variables at once \item Matrix::TsparseMatrix capability and the native
+#'   integration of stratification induce significant performance gains. \item
+#'   the ability to precalculate some time-consuming operations speeds up the
+#'   estimation at execution time. } }
+#'   
+#'   
+#' @return \itemize{ \item if \code{y} is not \code{NULL} (calculation step) : a
+#'   numerical vector of sizz the number of columns of y. \item if \code{y} is
+#'   \code{NULL} (precalculation step) : a list containing precalculated data:
+#'   \itemize{ \item \code{pik}: the numerical vector of first-order inclusion
+#'   probabilities. \item \code{A}: the numerical matrix denoted A in (Deville,
+#'   Tillé, 2005). \item \code{ck}: the numerical vector denoted ck2 in
+#'   (Deville, Tillé, 2005). \item \code{inv}: the inverse of \code{A \%*\%
+#'   Matrix::Diagonal(x = ck) \%*\% t(A)} \item \code{diago}: the diagonal term
+#'   of the variance estimator } }
+#'   
 #' @author Martin Chevalier (Insee)
-#'
+#'   
 #' @seealso \code{\link{block_matrix}} \code{\link{rescal}}
-#'
-#' @references Caron N., Deville J.-C., Sautory O. (1998), \emph{Estimation de précision
-#' de données issues d'enquêtes : document méthodologique sur le logiciel POULPE}, Insee
-#' working paper, n°9806
-#'
-#' Deville, J.-C. (1993), \emph{Estimation de la variance pour les enquêtes en deux phases},
-#' Manuscript, INSEE, Paris.
-#'
-#' Deville, J.-C., Tille, Y. (2005), "Variance approximation under balanced sampling",
-#' \emph{Journal of Statistical Planning and Inference}, 128, issue 2 569-591
-#'
-#' Rao, J.N.K (1975), "Unbiased variance estimation for multistage designs", \emph{Sankhya}, C n°37
+#'   
+#' @references Caron N., Deville J.-C., Sautory O. (1998), \emph{Estimation de
+#'   précision de données issues d'enquêtes : document méthodologique sur le
+#'   logiciel POULPE}, Insee working paper, n°9806
+#'   
+#'   Deville, J.-C. (1993), \emph{Estimation de la variance pour les enquêtes en
+#'   deux phases}, Manuscript, INSEE, Paris.
+#'   
+#'   Deville, J.-C., Tille, Y. (2005), "Variance approximation under balanced
+#'   sampling", \emph{Journal of Statistical Planning and Inference}, 128, issue
+#'   2 569-591
+#'   
+#'   Rao, J.N.K (1975), "Unbiased variance estimation for multistage designs",
+#'   \emph{Sankhya}, C n°37
 #'
 #' @examples require(sampling)
 #' set.seed(1)
