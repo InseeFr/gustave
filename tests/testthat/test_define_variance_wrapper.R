ee125fb869bb0fd4fc22021f550410c6e580364f
Martin Chevalier <martin.chevalier@insee.fr>
2017-09-11 23:23:04 +0200

    Test for the suppression of Matrix dependency in define_linearization_wrapper.

--- tests/testthat/test_define_variance_wrapper.R
+++ tests/testthat/test_define_variance_wrapper.R
@@ -16,11 +16,27 @@ test_that("variance_wrapper can be defined", {
     , regexp = NA)
 })
 
+# variance_wrapper(survey, quali)
+# 
+# library(microbenchmark)
+# microbenchmark(
+#   t(Matrix::fac2sparse(d[[1]]$data[[1]], giveCsparse = FALSE))  
+#   , model.matrix(~ . -1, model.frame(~ ., d[[1]]$data[[1]]))
+# )
+
 test_that("variance_wrapper works", {
   expect_error(
     variance_wrapper(survey, quanti, by = bynoNA)
     , regexp = NA)
 })
+
+test_that("point estimates work for qualitative variables", {
+  expect_equal(
+    variance_wrapper(survey, total(quali))$est
+    , as.vector(tapply(survey$w3, survey$quali, sum))
+  )
+})
+
 
 context("define_variance_wrapper - Function and data defined in another function")
 
@@ -48,7 +64,7 @@ test_that("varwrap_test can be defined", {
 
 test_that("varwrap_test works", {
   expect_error(
-    varwrap_test(survey, quanti)
+    varwrap_test(survey, quanti, quali)
     , regexp = NA)
 })
 
