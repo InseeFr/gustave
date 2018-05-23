e1ecc8c459633264039c02fd7c7dd85a3881249e
Martin Chevalier <martin.chevalier@insee.fr>
2018-05-23 13:59:58 +0200

    Rewrite, test and document utils exported functions

--- R/utils.R
+++ R/utils.R
@@ -1,54 +1,197 @@
 
 
-#' Efficient sum by group using the Matrix package
+#' Efficient by-group (weighted) summation
 #' 
-#' @description 
+#' @description \code{sumby()} performs an efficient and optionnaly weighted 
+#' by-group summation by using linear algebra and the Matrix package 
+#' capabilities. The by-group summation is performed through matrix cross-product
+#' of the y parameter (coerced to a matrix if needed) with a (very) sparse
+#' matrix bulit up using the by and the (optional) w parameters. 
+#' 
+#' Compared to base R, dplyr or data.table alternatives, this implementation 
+#' aims at being easier to use in a matrix-oriented context and can yield 
+#' efficiency gains when the number of columns becomes high.
+#' 
+#' @param y A (sparse) vector, a (sparse) matrix or a data.frame. 
+#' The object to perform by-groupe summation on. 
+#' @param by The factor variable defining the by-groups. Character variables
+#' are coerced to factors.
+#' @param w The optional weight to be used in the summation. 
+#' @param na_rm Should NA values in y be removed (ie treated as 0 in the summation) ? 
+#' Similar to na.rm argument in \code{\link[base]{sum}}, but TRUE by default. 
+#' If FALSE, NA values in y produce NA values in the result.
+#' @param keep_sparse When y is a sparse vector or a sparse matrix, should the result
+#' also be sparse ? FALSE by default. As \code{\link[Matrix]{sparseVector-class}} does
+#' not have a name attribute, when y is a sparseVector the result does not have any
+#' name (and a warning is cast).
+#' 
+#' @return A vector, a matrix or a data.frame depending on the type of y. If y is
+#' sparse and keep_sparse is TRUE, then the result is also sparse (without names
+#' when it is a sparse vector, see keep_sparse argument for details).
+#' 
+#' @author Martin Chevalier (Insee, French Statistical Institute)
+#' 
+#' @examples # Data generation
+#' set.seed(1)
+#' n <- 100
+#' p <- 10
+#' H <- 3
+#' y <- matrix(rnorm(n*p), ncol = p, dimnames = list(NULL, paste0("var", 1:10)))
+#' y[1, 1] <- NA
+#' by <- letters[sample.int(H, n, replace = TRUE)]
+#' w <- rep(1, n)
+#' w[by == "a"] <- 2
+#' 
+#' # Standard use
+#' sumby(y, by)
+#' 
+#' # Keeping the NAs
+#' sumby(y, by, na_rm = FALSE)
+#' 
+#' # With a weight
+#' sumby(y, by, w = w)
 #' 
 #' @export
+#' @import Matrix
 
-sumby <- function(y, by, w = NULL){
+sumby <- function(y, by, w = NULL, na_rm = TRUE, keep_sparse = FALSE){
 
-  isVector <- is.null(dim(y))
-  n <- NROW(y)
-  if(!is.null(w)) y <- y * w
-  if(anyNA(by)){
-    byNA <- is.na(by)
-    y <- y[!byNA, , drop = FALSE]
-    by <- by[!byNA]
-    n <- NROW(y)
+  # y <- V
+  
+  # Type of y
+  class_y <- class(y)
+  is_data.frame_y <- is.data.frame(y)
+  if(is_data.frame_y) y <- as.matrix(y)
+  is_sparse_y <- isTRUE(attr(class(y), "package") == "Matrix")
+  is_vector_y <- is.null(dim(y))
+  is_numeric_y <- is.numeric(if(!is_sparse_y) y else y@x)
+  if(!is_numeric_y) stop("y is not numeric (or not entirely).")
+  if(!is_sparse_y | is_vector_y) y <- methods::as(y, "sparseMatrix")
+  
+  # Weight, NA in y
+  if(is.null(w)) w <- rep(1, NROW(y))
+  if(!is.numeric(w)) stop("w is not numeric")
+  if(na_rm) y[is.na(y)] <- 0
+  
+  # NA in by
+  NA_in_by <- is.na(by)
+  if(any(NA_in_by)){
+    y <- y[!NA_in_by, , drop = FALSE]
+    by <- by[!NA_in_by]
+    w <- w[!NA_in_by]
   }
+  
+  # Matrix cross-product
   by <- as.factor(by)
-  x <- block_matrix(rep(TRUE, n), by)$y
-  r <- Matrix::t(x) %*% y
+  x <- block_matrix(w, by)$y
+  colnames(x) <- levels(by)
+  r <- crossprod(x, y)
 
-  if(isVector){
-    r <- as.vector(r)
-    names(r) <- levels(by)
+  # Type of r
+  if(!is_sparse_y | !keep_sparse){
+    r <- if(is_vector_y) stats::setNames(as.vector(r), rownames(r)) else as.matrix(r)
   }else{
-    if(is.matrix(y)) r <- as.matrix(r)
-    rownames(r) <- levels(by)
+    if(is_vector_y) warning("sparseVector can't have names, hence the output won't have names.")
+    r <- methods::as(r, class_y)
   }
-
-  return(r)
-
+  if(is_data.frame_y) r <- as.data.frame(r)
+  
+  r
+  
 }
 
 
-#' Expand a matrix with zeros based on rownames matching
-#' @export
-
-add0 <- function(y, rownames){
+#' Expand a matrix or a data.frame with zeros based on rownames matching
+#' 
+#' @description For a given two-dimensional object with rownames, (sparse) 
+#' matrix or data.frame and a character vector, \code{add0()} produces
+#' a corresponding object whose rownames match the character vector 
+#' with zeros on the additional rows.
+#' 
+#' This function is an easy-to-use and reliable way to reintroduce 
+#' non-responding in the variance estimation process (after the 
+#' non-response phase is taken into account).
+#' 
+#' @param y A (sparse) matrix or a data.frame. The object to add zeros to. 
+#' @param rownames A character vector (other types are coerced to character).
+#' The character vector giving the rows of the produced object.
+#' @param remove Should rows of y whose name do not appear in the rownames
+#' argument be removed ? TRUE by default, a warning is shown when rows are
+#' removed.
+#' 
+#' @return A (sparse) matrix or data.frame depending on the type of y.
+#' 
+#' @author Martin Chevalier (Insee, French Statistical Institute)
+#' 
+#' @examples # Data generation
+#' set.seed(1)
+#' n <- 10
+#' p <- 2
+#' y <- matrix(1:(n*p), ncol = p, dimnames = list(sample(letters, n)))
+#' y[c(3, 8, 12)] <- NA
+#' rownames <- letters
+#' 
+#' # Standard use
+#' add0(y, rownames) 
+#' 
+#' # Use when rownames in y do not match 
+#' # any element in the rownames argument
+#' rownames(y)[1:3] <- toupper(rownames(y)[1:3])
+#' add0(y, rownames)
+#' add0(y, rownames, remove = FALSE)
+#' 
+#' @import Matrix
+#' @export 
+#' 
+add0 <- function(y, rownames, remove = TRUE){
+  
+  # y <- m; rownames <- letters
+  
+  # Type of y
+  class_y <- class(y)
+  is_data.frame_y <- is.data.frame(y)
+  if(is_data.frame_y) y <- as.matrix(y)
+  if(is.null(dim(y)))
+    stop("y must be a (sparse) matrix or a data.frame.")
+  if(is.null(rownames(y)))
+     stop("y must have rownames in order to be used in add0().")
+  is_sparse_y <- isTRUE(attr(class(y), "package") == "Matrix")
+  is_numeric_y <- is.numeric(if(!is_sparse_y) y else y@x)
+  if(!is_numeric_y) stop("y is not numeric (or not entirely).")
+  
+  # Prepare rownames argument
+  rownames <- rownames[!is.na(rownames)]
+  rownames <- as.character(rownames)
+  
+  # Expand y with 0 in order to get an object whose rownames 
+  # are the character argument rownames (in the same order)
   compl <- setdiff(rownames, rownames(y))
-  compl <- matrix(
-    0, nrow = length(compl), ncol = NCOL(y), dimnames = list(compl)
-  )
-  rbind(y, compl)[rownames, , drop = FALSE]
+  if(!is_sparse_y){
+    r <- rbind(y, matrix(0, nrow = length(compl), ncol = NCOL(y), dimnames = list(compl)))
+    r <- as.data.frame(r)
+  }else{
+    r <- rbind(y, Matrix(0, nrow = length(compl), ncol = NCOL(y), dimnames = list(compl, NULL)))
+    r <- methods::as(r, class_y)
+  }
+
+  # Remove rows that do not match any element in rownames 
+  # if remove is TRUE
+  if(remove){
+    if(length(setdiff(rownames(y), rownames)))
+      warning("The name of some rows in y do not match any element in the rownames argument. These rows are removed from the result (use remove = FALSE to change this behaviour).")
+    o <- rownames    
+  }else o <- order(rownames(r))
+  
+  r[o, , drop = FALSE]
+  
 }
 
 
 
 
 # Unexported (and undocumented) functions
+
+#' @import Matrix
 
 block_matrix <- function(y, by){
   # y <- as(Matrix(TRUE, ncol = 10, nrow = length(rowby)), "TsparseMatrix"); by <- rowby; p <- 2
@@ -65,7 +208,8 @@ block_matrix <- function(y, by){
   bycol <- if(is.factor(byrow)) as.factor(bycol) else methods::as(bycol, class(byrow))
   return(list(y = y, byrow = byrow, bycol = bycol))
 }
-
+# TODO: export a matrix with rowby/colby attributes instead of a list, 
+# add an option for row/colnames adjustment (with a given separator)
 
 change_enclosing <- function(FUN, envir = environment(FUN)){
   eval(parse(text = deparse(FUN)), envir = envir)
--- tests/testthat/test_utils.R
+++ tests/testthat/test_utils.R
@@ -2,6 +2,93 @@
 
 context("utils")
 
+
+# sumby()
+
+set.seed(1)
+n <- 10
+v <- as.numeric(1:n)
+v[6] <- NA
+V <- sparseVector(v, seq_along(v), n)
+m <- matrix(c(v, v), ncol = 2)
+colnames(m) <- c("variable1", "variable2")
+M <- Matrix(m)
+df <- as.data.frame(m)
+by <- sample(letters[1:3], 10, replace = TRUE)
+w <- rep(2, n)
+by_NA <- by
+by_NA[c(2, 8)] <- NA
+
+test_that("sumby() works", {
+  
+  # Standard behaviour
+  r <- sapply(split(v, by), sum, na.rm = TRUE)
+  expect_equal(sumby(v, by), r)
+  expect_equal(sumby(V, by), r)
+  expect_equal(sumby(m, by), matrix(c(r, r), ncol = 2, dimnames = list(names(r), colnames(m))))
+  expect_equal(sumby(M, by), matrix(c(r, r), ncol = 2, dimnames = list(names(r), colnames(m))))
+  expect_equal(sumby(M, by, keep_sparse = TRUE), Matrix(c(r, r), ncol = 2, dimnames = list(names(r), colnames(m))))
+  expect_equal(sumby(df, by), data.frame(variable1 = r, variable2 = r, row.names = names(r)))
+  expect_equal(sumby(v, by, w), r * 2)
+  
+  # Standard behaviour without removing the NA values
+  r <- sapply(split(v, by), sum)
+  expect_equal(sumby(v, by, na_rm = FALSE), r)
+  expect_equal(sumby(V, by, na_rm = FALSE), r)
+  expect_equal(sumby(m, by, na_rm = FALSE), matrix(c(r, r), ncol = 2, dimnames = list(names(r), colnames(m))))
+  expect_equal(sumby(M, by, na_rm = FALSE), matrix(c(r, r), ncol = 2, dimnames = list(names(r), colnames(m))))
+  expect_equal(sumby(M, by, na_rm = FALSE, keep_sparse = TRUE), Matrix(c(r, r), ncol = 2, dimnames = list(names(r), colnames(m))))
+  expect_equal(sumby(df, by, na_rm = FALSE), data.frame(variable1 = r, variable2 = r, row.names = names(r)))
+  expect_equal(sumby(v, by, w, na_rm = FALSE), r * 2)
+  
+  # Standard behaviour with NA value in the by variable
+  r <- sapply(split(v, by_NA), sum, na.rm = TRUE)
+  expect_equal(sumby(v, by_NA), r)
+  expect_equal(sumby(V, by_NA), r)
+  expect_equal(sumby(m, by_NA), matrix(c(r, r), ncol = 2, dimnames = list(names(r), colnames(m))))
+  expect_equal(sumby(M, by_NA), matrix(c(r, r), ncol = 2, dimnames = list(names(r), colnames(m))))
+  expect_equal(sumby(M, by_NA, keep_sparse = TRUE), Matrix(c(r, r), ncol = 2, dimnames = list(names(r), colnames(m))))
+  expect_equal(sumby(df, by_NA), data.frame(variable1 = r, variable2 = r, row.names = names(r)))
+  expect_equal(sumby(v, by_NA, w), r * 2)
+  
+  # Error messages
+  expect_error(sumby(letters[seq_along(by)], by), regexp = "y is not")
+
+})
+
+
+# add0()
+
+set.seed(1)
+n <- 10
+p <- 2
+m <- matrix(1:(n*p), ncol = p, dimnames = list(sample(letters, n)))
+m[c(3, 8, 12)] <- NA
+M <- as(m, "TsparseMatrix")
+rownames(M) <- rownames(m)
+df <- as.data.frame(m)
+
+test_that("add0() works", {
+  
+  # Standard behaviour
+  expect_error(add0(m, letters), regexp = NA)
+  expect_error(add0(M, letters), regexp = NA)
+  expect_error(add0(df, letters), regexp = NA)
+  expect_error(add0(m, as.factor(letters)), regexp = NA)
+  
+  # Error and warning messages
+  expect_error(add0(m[, 1], letters), regexp = "y must be")
+  expect_error(add0(unname(m), letters), regexp = "y must have")
+  expect_error(add0(matrix(letters[m], ncol = 2, dimnames = list(rownames(m))), letters), regexp = "y is not numeric")
+  tmp <- m
+  rownames(tmp)[1:3] <- toupper(rownames(tmp)[1:3])
+  expect_warning(add0(tmp, letters), regexp = "The name of some rows")
+
+})
+
+
+# assign_all()
+
 a <- 1
 fun <- function(){}
 clos <- (function(){
