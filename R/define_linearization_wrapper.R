04eed4eeda571f61e42ed1314162cc8140aa8c89
Martin Chevalier <martin.chevalier@insee.fr>
2018-05-27 21:58:00 +0200

    Suppress technical_arg from linerization wrappers Phase 1 : goal reached but label capability and magrittr support no more functional Phase 2 : call display properly works again

--- R/define_linearization_wrapper.R
+++ R/define_linearization_wrapper.R
@@ -26,15 +26,17 @@ define_linearization_wrapper <- function(
     stop("A weight argument must be provided in order to create a linearization wrapper.")
 
   # Step 1 : Create the linearization wrapper
-  linearization_wrapper <- function(by = NULL, where = NULL, technical_arg){
+  linearization_wrapper <- function(by = NULL, where = NULL, ...){
     
     # Step 1.1 : Capture and modify the call
     call <- match.call(expand.dots = TRUE)
     call_list <- as.list(call)[-1]
-    call_list$technical_arg <- c(technical_arg, list(
+    call_list <- c(call_list, list(
       allow_factor = allow_factor, arg_type = arg_type
       , arg_not_affected_by_domain = arg_not_affected_by_domain
     ))
+    call_display_arg <- c(1, which(names(call) %in% setdiff(names(formals(sys.function())), "...") & !sapply(call, is.null)))
+    call_display <- paste(deparse(call[call_display_arg], width.cutoff = 500L), collapse = "")
     
     # Step 1.2 : Proceeed to standard preparation
     preparation <- do.call(standard_preparation, call_list)
@@ -42,13 +44,14 @@ define_linearization_wrapper <- function(
     d <- lapply(preparation, function(i) list(
       preparation = i
       , display = list(metadata = list(standard = data.frame(
-        label = technical_arg$label
-        , call = paste(deparse(call[!(names(call) %in% c("technical_arg", arg_type$weight)) | sapply(call, is.null)], width.cutoff = 500L), collapse = "")
-        , mod = i$metadata$mod, by = i$metadata$by
-        , stringsAsFactors = FALSE
+        # label = label,
+        call = call_display,
+        mod = i$metadata$mod, by = i$metadata$by, 
+        stringsAsFactors = FALSE
       )))
     ))
     # TODO: directly produce the display part within standard_preparation() (save for call)
+    # TODO: reactivate label capability
     
     # Step 1.3 : Evaluate the linearization functions
     d <- lapply(d, function(i){
@@ -63,7 +66,10 @@ define_linearization_wrapper <- function(
   }
 
   # Step 2 : Modify linearization_wrapper formals
-  formals(linearization_wrapper) <- c(formals(linearization_function), formals(linearization_wrapper))
+  formals(linearization_wrapper) <- c(
+    formals(linearization_function)[setdiff(names(formals(linearization_function)), arg_type$weight)], 
+    formals(linearization_wrapper)
+  )
 
   # Step 3 : Include aobjects in linearization_wrapper enclosing environment
   e <- new.env(parent = globalenv())
@@ -75,23 +81,26 @@ define_linearization_wrapper <- function(
   return(linearization_wrapper)
 }
 
-# standard_preparation() function
-standard_preparation <- function(..., by = NULL, where = NULL, technical_arg){
+standard_preparation <- function(..., 
+                                 by = NULL, where = NULL, 
+                                 data, label, evaluation_envir, execution_envir, 
+                                 allow_factor, arg_type, arg_not_affected_by_domain
+  ){
 
   # Step 1 : Evaluation
-  eval_data <- eval(technical_arg$data, technical_arg$evaluation_envir)
+  eval_data <- eval(data, evaluation_envir)
   expr <- eval(substitute(alist(...)))
   d <- list(list(
-    data = lapply(expr[names(expr) %in% technical_arg$arg_type$data], eval, envir = eval_data, enclos = technical_arg$evaluation_envir)
-    , weight = lapply(expr[names(expr) %in% technical_arg$arg_type$weight], eval, envir = eval_data, enclos = technical_arg$evaluation_envir)
-    , param = if(any(names(expr) %in% technical_arg$arg_type$param)) expr[names(expr) %in% technical_arg$arg_type$param] else NULL
+    data = lapply(expr[names(expr) %in% arg_type$data], eval, envir = eval_data, enclos = evaluation_envir)
+    , weight = lapply(expr[names(expr) %in% arg_type$weight], eval, envir = eval_data, enclos = evaluation_envir)
+    , param = if(any(names(expr) %in% arg_type$param)) expr[names(expr) %in% arg_type$param] else NULL
   ))
   if(all(sapply(d[[1]]$data, is.null))) return(NULL)
 
   # Step 2 : Handling factors and character variables in data
   fac <- sapply(d[[1]]$data, function(i) is.character(i) || is.factor(i))
   if(sum(fac) > 0){
-    if(technical_arg$allow_factor && length(d[[1]]$data) == 1){
+    if(allow_factor && length(d[[1]]$data) == 1){
       tmp <- d[[1]]$data[[1]]
       if(is.character(tmp)) tmp <- as.factor(tmp)
       tmp <- droplevels(tmp)
@@ -115,8 +124,12 @@ standard_preparation <- function(..., by = NULL, where = NULL, technical_arg){
   by <- if(!byNULL){
     as.factor(eval(substitute(by), eval_data))
   }else{
-    t <- rep(1L, n); levels(t) <- "1"; class(t) <- "factor"; t
+    tmp <- rep(1L, n)
+    levels(tmp) <- "1"
+    class(tmp) <- "factor"
+    tmp
   }
+  # spy <<- d; stop("Control point")
   if(!is.null(substitute(where))){
     by[!as.logical(eval(substitute(where), eval_data))] <- NA
     by <- droplevels(by)
@@ -129,10 +142,10 @@ standard_preparation <- function(..., by = NULL, where = NULL, technical_arg){
   d <- unlist(lapply(seq_along(bypos), function(i){
     lapply(d, function(j) list(
       data = lapply(stats::setNames(seq_along(j$data), names(j$data)), function(k){
-        if(names(j$data)[k] %in% technical_arg$arg_not_affected_by_domain) j$data[[k]] else j$data[[k]][bypos[[i]]]
+        if(names(j$data)[k] %in% arg_not_affected_by_domain) j$data[[k]] else j$data[[k]][bypos[[i]]]
       })
       , weight = lapply(stats::setNames(seq_along(j$weight), names(j$weight)), function(k){
-        if(names(j$weight)[k] %in% technical_arg$arg_not_affected_by_domain) j$weight[[k]] else j$weight[[k]][bypos[[i]]]
+        if(names(j$weight)[k] %in% arg_not_affected_by_domain) j$weight[[k]] else j$weight[[k]][bypos[[i]]]
       }), param = j$param
       , metadata = c(j$metadata, list(
         by = if(byNULL) NA else names(bypos)[i]
@@ -144,7 +157,6 @@ standard_preparation <- function(..., by = NULL, where = NULL, technical_arg){
 
 }
 
-# standard_display_function()
 standard_display_function <- function(i, alpha){
   d <- i$metadata$standard
   if(!is.null(i$metadata$n)) d$n <- i$metadata$n
