83ca4a5895ea9f75b9b873219dcae89f224cb829
Martin Chevalier <martin.chevalier@insee.fr>
2018-05-28 17:47:54 +0200

    Add dev.R file to git

---
+++ misc/dev.R
@@ -0,0 +1,11 @@
+
+
+variance_wrapper <- define_variance_wrapper(
+  variance_function = function(y) abs(colSums(y)), 
+  reference_id = ict_survey$firm_id, 
+  default = list(id = "firm_id", weight = "w_calib", stat = "mean")
+)
+tmp <- variance_wrapper(ict_survey, "blabla" = mean(speed_quanti, by = division), display = FALSE)
+str(tmp)
+
+variance_wrapper(ict_survey, blabla = speed_quali, by = division)
