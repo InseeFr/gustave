54e02751cf3917ca67ccbabc593f70493de58bb9
Martin Chevalier <martin.chevalier@insee.fr>
2018-06-08 18:06:12 +0200

    New approach for lin function

--- misc/lin.R
+++ misc/lin.R
@@ -6,45 +6,44 @@
 #' package capabilities with standard evaluation.
 #' @param ... Arguments to be passed to the linearization
 #' function, as variable names (character vectors of length 1)
-#' @param stat,.stat Statistic to be used for linearization,
+#' @param stat, Statistic to be used for linearization,
 #' either as a function or as a function name (character vector
 #' of length 1)
-#' @param by,.by Domains to split accross, either as an R expression
+#' @param by, Domains to split accross, either as an R expression
 #' or as a variable name (character vector of length 1)
-#' @param where,.where Domain restriction, either as an R expression
+#' @param where, Domain restriction, either as an R expression
 #' or as a variable name (character vector of length 1)
 #' 
 #' @output The corresponding unevaluated function call.
 
-lin <- function(..., 
-                stat = NULL,
+is_variable_name <- function(x) is.character(x) && length(x) == 1
+
+lin <- function(stat,
+                ..., 
                 by = NULL,
                 where = NULL
 ){
   
-  # Step 1: Controls
-  if(is.null(stat) && is.null(.stat))
-    stop("Either stat or .stat must be not NULL.")
-  if(!is.null(stat) && !is.null(.stat))
-    stop("stat and .stat must not be not NULL simultaneously.")
-  if(!is.null(by) && !is.null(.by))
-    stop("stat and .stat must not be not NULL simultaneously.")
-  if(!is.null(where) && !is.null(.where))
-    stop("stat and .stat must not be not NULL simultaneously.")
-  if(!is.null(.stat) && (!is.character(.stat) || !length(.stat) == 1))
-    stop(".stat must be a character vector of length 1.")
-  if(!is.null(.by) && (!is.character(.by) || !length(.by) == 1))
-    stop(".by must be a character vector of length 1.")
-  if(!is.null(.where) && (!is.character(.where) || !length(.where) == 1))
-    stop(".where must be a character vector of length 1.")
+  parent_frame <- parent.frame()
   
-  # Step 2: Coertion
-  if(!is.null(stat)) .stat <- deparse(substitute(stat))
-  if(!is.null(.by)) .by <- as.symbol(.by)
+  stat <- if(is.function(stat)) substitute(stat) else as.symbol(stat)
   
+  arg <- eval(substitute(alist(...)), envir = parent_frame)
+  spy <<- arg; stop()
+  if(any(!sapply(arg, is_variable_name)))
+    stop("All arguments passed on to the linearization wrapper (through \"...\") must be variables names (character vectors of length 1)")
+  arg <- lapply(arg, as.symbol)
   
-  return(.stat)
+  by <- if(exists(deparse(substitute(by))) && is_variable_name(by))
+    as.symbol(by) else substitute(by)
+  where <- if(exists(deparse(substitute(by))) && is_variable_name(by))
+    as.symbol(where) else substitute(where)
+  
+  as.call(c(list(stat), arg, list(by = by, where = where)))
 
+  
 }
 
-lin("big_data", stat = "mean")
+# lin("mean", "speed_quanti")
+
+sapply(c("speed_quanti", "speed_quali"), function(var) lin(stat = "mean", var))
