5151a89040436e980b3bac82ea8205606f465faa
Martin Chevalier <martin.chevalier@insee.fr>
2018-06-18 11:49:51 +0200

    Set up the files for define_simple_wrapper

---
+++ R/simple.R
@@ -0,0 +1,24 @@
+
+
+
+#' Define a simple variance wrapper
+#'
+#' @description define_simple_wrapper is a convenience wrapper
+#' for define_variance_wrapper for the simplest yet frequent cases: 
+#' \itemize{\item (stratified) simple random sampling \item non-response
+#' correction (if any) through reweighting in Homogeneous response groups 
+#' (HRG) \item calibration (if any)}
+#' It performs technical and statistical checks and (eventually) produces
+#' a ready-to-use variance wrapper adapted for such cases.
+#'
+#' @export 
+define_simple_wrapper <-function(){
+  
+}
+
+# Unexported (and undocumented) function
+var_simple <- function(){
+  
+  
+  
+}
